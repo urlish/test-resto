b800c8f8e307eb4911df928bfd81a121181b6859
urlish <ulrich@netsanslimites.com>
2016-10-03 17:20:07 +0200

    nvo01

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={			
-	'ApiUrl' : "YOUR_API_URL",	
-	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
-	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
-	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
+	'ApiUrl' : "http://www.appeti100.com/cg/merchantapp/api",	
+	'DialogDefaultTitle' : "Appeti100-Resto",
+	'pushNotificationSenderid' : "651716383412",
+	'APIHasKey' : "1024cf428f3a45888dd8e7d3a5ee9a10"
 };
