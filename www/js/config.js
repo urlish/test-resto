419813b8ca798bf50183f54711e3b2ada8d4bafa
urlish <ulrich@netsanslimites.com>
2016-10-03 15:55:33 +0200

    test

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "YOUR_API_URL",	
+	'DialogDefaultTitle' : "YOUR_OWN_DIALOG_TITLE",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'APIHasKey' : "YOUR API HASH KEY (OPTIONAL)"
+};
